918cab4150f109f561667a9a2fa78c6eacbc5e5f
RiceEnjoyer21 <rice.old21@gmail.com>
2025-04-21 13:06:41 +0500

    push

--- bocchi_back/users/models.py
+++ bocchi_back/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-class myAccoutManager(BaseUserManager)
+class myAccoutManager(BaseUserManager):
+    
 
 class User(models.Model):
     username = models.CharField(max_length=150, unique=True)
