2f02272f7a46dcb494700e5ba323cc0024d3b7c2
RiceEnjoyer21 <rice.old21@gmail.com>
2025-04-21 13:04:10 +0500

    push

--- bocchi_back/api/models.py
+++ bocchi_back/api/models.py
@@ -1,14 +1,5 @@
 from django.db import models
 
-class User(models.Model):
-    username = models.CharField(max_length=150, unique=True)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.username
-
 class Group(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
--- bocchi_back/users/models.py
+++ bocchi_back/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+class myAccoutManager(BaseUserManager)
+
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+
+
+
+    def __str__(self):
+        return self.username
+
 # Create your models here.
