9c7a618355863da2f5df0cbc88134e6b886ac853
Genpactal <dayanospanov06@gmail.com>
2025-04-23 18:12:00 +0500

    users final(maybe)

--- bocchi_back/users/urls.py
+++ bocchi_back/users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterView, MyProfileView, TestProtectedView
 
 urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('me/', MyProfileView.as_view(), name='my_profile'),
+    path('test/', TestProtectedView.as_view(), name='protected_test'),
 ]
