0095a93600a6a868ffc97e8d9f97edaac065df69
Genpactal <dayanospanov06@gmail.com>
2025-04-24 11:19:01 +0500

    "little fix"

--- bocchi_back/api/models.py
+++ bocchi_back/api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Ticket(models.Model):
-    ticket_id = models.CharField(max_length=255, unique=True)
+    id = models.AutoField(primary_key=True)
     event_name = models.CharField(max_length=255)
     event_date = models.DateTimeField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -11,10 +11,13 @@ class Ticket(models.Model):
         return f"{self.event_name} - {self.event_date}"    
     
 class Merch(models.Model):
+    id = models.AutoField(primary_key=True)
     item_name = models.CharField(max_length=255)
     item_description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     purchased_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='merch_images/', blank=True, null=True)
+
 
     def __str__(self):
         return self.item_name   
--- bocchi_back/groups/models.py
+++ bocchi_back/groups/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Group(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='group_images/', blank=True, null=True)
